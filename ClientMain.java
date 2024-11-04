ba466deca5e3cd285850868b385abf3f8876139a
sanketkalhapure <160130376+sanketkalhapure@users.noreply.github.com>
2024-11-04 21:15:58 +0530

    Add files via upload

---
+++ ClientMain.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package client;
+
+/**
+ *
+ * @author User
+ */
+public class ClientMain {
+    public static void main(String[] args) {
+        Client c=new Client();
+        
+        c.setIoStreams();
+    }
+    
+}
